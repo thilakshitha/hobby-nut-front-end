02fca76f529683f518d1b4e483d06a9bd7d7e782
thilakshitha <163345595+thilakshitha@users.noreply.github.com>
2024-09-05 18:48:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//get data
+const nameInput = document.querySelector("#name");
+const email= document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelector(".error");
+
+//validate data
+function validateForm(){
+          console.log("hello world!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+}
